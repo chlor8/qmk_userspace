b92ab49f03a62260c613e7c2642ac9f4b815d0a2
chlor8 <chlor@niflheim.com>
2025-11-02 21:13:32 -0600

    update

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/chlor8/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/chlor8/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q   ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                     KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P ,
   GUI_A ,  ALT_S   ,  SHIFT_D  ,   CTRL_F ,   KC_G ,                                     KC_H,   CTRL_J ,  SHIFT_K ,   ALT_L ,GUI_QUOT,
   KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B ,                                     KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_UNDERSCORE ,
-                    KC_ENTER, TAB_NAV , KC_BSPC,                                              KC_SPACE, SYM, FKEYS
+                    FKEYS, TAB_NAV , KC_BSPC,                                              KC_SPACE, SYM, FKEYS
 ),
 
 [_NAV] = LAYOUT_split_3x5_3(
